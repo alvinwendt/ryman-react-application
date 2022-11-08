d256cfffb57a50a7cae6dcceb11789acc4470c11
Danny Herrmann <dannyherrmann91@gmail.com>
2022-11-08 07:26:03 -0600

    event styling

--- src/components/views/ApplicationViews.js
+++ src/components/views/ApplicationViews.js
@@ -1,10 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import { Events } from "../events/Events";
+
 export const ApplicationViews = () => {
   const localRymanUser = localStorage.getItem("ryman_user");
   const rymanUserObject = JSON.parse(localRymanUser);
 
-  if (rymanUserObject.staff) {
-    return "<h1>Staff Views</h1>";
-  } else {
-    return "<h1>Patron Views</h1>";
-  }
+  return (
+    <Routes>
+      <Route path="/" element={
+        <>
+          <Events />
+        </>
+      }></Route>
+    </Routes>
+  )
 };
