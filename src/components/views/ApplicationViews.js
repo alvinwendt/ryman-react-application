04bec50420796be9df67fed9d798b638a3ae5fd2
Cristi Neames <crisneames@gmail.com>
2022-11-08 18:53:25 -0600

    created EventsForm.js

--- src/components/views/ApplicationViews.js
+++ src/components/views/ApplicationViews.js
@@ -1,10 +1,32 @@
+import { Outlet, Route, Routes } from 'react-router-dom';
+import { EventsForm } from '../EventsForm';
+
 export const ApplicationViews = () => {
-  const localProjectUser = localStorage.getItem("project_user");
+  const localProjectUser = localStorage.getItem('project_user');
   const projectUserObject = JSON.parse(localProjectUser);
 
-  if (projectUserObject.staff) {
-    return "<h1>Staff Views</h1>";
-  } else {
-    return "<h1>Patron Views</h1>";
-  }
+  //   if (projectUserObject.staff) {
+  //     return "<h1>Staff Views</h1>";
+  //   } else {
+  //     return "<h1>Patron Views</h1>";
+  //   }
+  // };
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <>
+            <h1>Ryman</h1>
+
+            <Outlet />
+          </>
+        }
+      >
+        {/* <Route path="products" element={<EventsContainer />} /> */}
+
+        <Route path="events/create" element={<EventsForm />} />
+      </Route>
+    </Routes>
+  );
 };
