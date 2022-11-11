39af60c90bd72bae4d18e4007648306a7d08508d
Danny Herrmann <dannyherrmann91@gmail.com>
2022-11-10 20:33:50 -0600

    adding date-fns

--- src/components/events/Event.js
+++ src/components/events/Event.js
@@ -1,10 +1,18 @@
+import { format } from "date-fns"
+
 export const Event = ({ eventType, eventName, eventImage, eventDate, eventTime }) => {
+
+    const convertEventDate = new Date(eventDate)
+    console.log(`converted event date`,convertEventDate)
+    const eventDateFormatted = format(convertEventDate, 'LLLL d, yyyy')
+    const convertEventTime = new Date(eventTime)
+    console.log(`new event time`,convertEventTime)
 
     return <section className="event">
         <div className="eventDetails">
             <p className="eventType">{eventType}</p>
             <h3 className="eventName">{eventName}</h3>
-            <p className="eventDateTime">{eventDate} at {eventTime}</p>
+            <p className="eventDateTime">{eventDateFormatted} at {eventTime}</p>
             <button className="moreInfo">MORE INFO</button>
         </div>
         <div className="eventImage">
